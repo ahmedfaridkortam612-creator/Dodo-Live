b250ef464973aa170ad7a6ac237a7bcf570e38cd
ahmedfaridkortam612-creator <ahmedfaridkortam612@gmail.com>
2026-09-16 19:13:31 +0300

    Add AppColors class with color constants

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color magenta = Color(0xFFE91E63);
+  static const Color gold = Color(0xFFFFD700);
+  static const Color surface = Color(0xFF1E1E1E);
+  static const Color textMuted = Color(0xFF9E9E9E);
+}
