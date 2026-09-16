0e11aea2826694f3cbd29c6d38f14f88e824ad00
ahmedfaridkortam612-creator <ahmedfaridkortam612@gmail.com>
2026-09-16 19:18:01 +0300

    Create feed_item_model.dart

---
+++ lib/features/home/feed_item_model.dart
@@ -0,0 +1,13 @@
+class FeedItemModel {
+  final String id;
+  final String title;
+  final String streamerName;
+  final int viewersCount;
+
+  const FeedItemModel({
+    required this.id,
+    required this.title,
+    required this.streamerName,
+    required this.viewersCount,
+  });
+}
