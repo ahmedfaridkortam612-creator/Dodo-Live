bcf5682c0cff60604978de9b29e51b37080f9032
ahmedfaridkortam612-creator <ahmedfaridkortam612@gmail.com>
2026-09-18 09:52:26 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ import 'screens/live_room_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    debugPrint("Firebase initialization error: $e");
+  }
   runApp(const DodoLiveApp());
 }
 
